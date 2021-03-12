cb923a54d294fdcb4d442476bb79672fe090683c
Giuseppe Lana <giuseppe.lana27@outlook.it>
2021-03-12 16:41:45 +0100

    aggiunte funzioni per scorrere i bullet del nav

--- js/main.js
+++ js/main.js
@@ -6,12 +6,29 @@ $(function() {
   next.click(
     function (){
       nextImg();
+      nextBull();
     }
   );
   // richiamo funzione per scorrere all'immagine precedente
   var prev = $(".prev");
   prev.click(
     function (){
+      prevImg();
+      prevBull();
+    }
+  );
+  // richiamo funzioni per scorrere i bullets
+  var nextBullet = $(".nav i");
+  nextBullet.click(
+    function(){
+      nextBull();
+      nextImg();
+    }
+  );
+  var prevBullet = $(".nav i");
+  prevBullet.click(
+    function(){
+      prevBull();
       prevImg();
     }
   );
@@ -23,7 +40,7 @@ function nextImg(){
   var imgActive = $(".images img.active");
   var firstImg = $(".images img.first");
   imgActive.removeClass("active");
-  if (imgActive.hasClass("last") == true ){
+  if (imgActive.hasClass("last") == true){
     firstImg.addClass("active");
   } else {
     imgActive.next().addClass("active");
@@ -34,9 +51,31 @@ function prevImg(){
   var imgActive = $(".images img.active");
   var lastImg = $(".images img.last");
   imgActive.removeClass("active");
-  if (imgActive.hasClass("first") == true ){
+  if (imgActive.hasClass("first") == true){
     lastImg.addClass("active");
   } else {
     imgActive.prev().addClass("active");
+  }
+}
+
+// creo funzione per scorrere i bullets
+function nextBull(){
+  var bullet = $(".nav i.active");
+  var firstBullet = $(".nav i.first");
+  bullet.removeClass("active");
+  if (bullet.hasClass("last") == true){
+    firstBullet.addClass("active");
+  } else {
+    bullet.next().addClass("active");
+  }
+}
+function prevBull(){
+  var bullet = $(".nav i.active");
+  var lastBullet = $(".nav i.last");
+  bullet.removeClass("active");
+  if (bullet.hasClass("first") == true){
+    lastBullet.addClass("active");
+  } else {
+    bullet.prev().addClass("active");
   }
 }
