a0a08343b2e5998000cefe8afd34900a74e96a06
Giuseppe Lana <giuseppe.lana27@outlook.it>
2021-03-12 15:45:48 +0100

    inserita seconda funzione per scorrere all'immagine precedente

--- js/main.js
+++ js/main.js
@@ -8,8 +8,13 @@ $(function() {
       nextImg();
     }
   );
-
-  
+  // richiamo funzione per scorrere all'immagine precedente
+  var prev = $(".prev");
+  prev.click(
+    function (){
+      prevImg();
+    }
+  );
 }
 );
 
@@ -22,5 +27,16 @@ function nextImg(){
     firstImg.addClass("active");
   } else {
     imgActive.next().addClass("active");
+  }
+}
+// creo funzione per scorrere all'immagine precedente
+function prevImg(){
+  var imgActive = $(".images img.active");
+  var lastImg = $(".images img.last");
+  imgActive.removeClass("active");
+  if (imgActive.hasClass("first") == true ){
+    lastImg.addClass("active");
+  } else {
+    imgActive.prev().addClass("active");
   }
 }
